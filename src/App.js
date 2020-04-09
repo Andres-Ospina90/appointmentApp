d35c5b6d60d0cc37692274100b4ab4abfb40d08b
“Andres” <andres.bio360@gmail.com>
2020-04-09 19:18:42 +0200

    input values set in a local and a global (app.js), validation form and alert added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 
 import AppointmentForm from './components/AppointmentForm';
 import AppointmentList from './components/AppointmentList';
 
 function App() {
+	// Arreglo de citas
+
+	const [citas, setCitas] = useState([]);
+
+	const handleAppointments = (cita) => {
+		setCitas([...citas, cita]);
+	};
+
+	useEffect(() => {
+		console.log(citas);
+	}, [citas]);
+
 	return (
 		<Fragment>
 			<h1>Administrador de Pacientes</h1>
 			<div className='container-fluid'>
 				<div className='row'>
-					<div className='col-sm-3 offset-sm-3'>
-						<AppointmentForm />
+					<div className='col-sm-4 offset-sm-2'>
+						<AppointmentForm handleAppointments={handleAppointments} />
 					</div>
-					<div className='col-sm-3 '>
+					<div className='col-sm-4 '>
 						<AppointmentList />
 					</div>
 				</div>
